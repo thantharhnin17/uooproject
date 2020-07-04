ee8b02af71015d5ab255fc4744866023ef412896
thantharhnin17 <thanthark88@gmail.com>
2020-07-03 16:32:21 -0800

    added header file

---
+++ header.php
@@ -0,0 +1,53 @@
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary navbar-custom fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="img/oxford.png" width="20%"></a>
+    </div>
+    <div class="navbar-collapse collapse">
+
+        <ul  id="top-menu" class="nav navbar-nav navbar-right ">
+			<li><a href="index.php" class="navbar-text">Home</a></li>
+		    <li><a href="application.php" class="navbar-text">Online Application</a></li>
+            <li><a href="login.php" class="navbar-text">Login</a></li>
+		</ul>
+			
+    </div>
+  </div>
+</nav>
+<div class="container pt-0">
+    <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+        <!-- Carousel indicators -->
+        <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <!-- Wrapper for carousel items -->
+        <div class="carousel-inner">
+            <div class="carousel-item active" >
+                <img src="img/oxbanner.jpg"  id="imageslider">
+                
+            </div>
+            <div class="carousel-item">
+                <img src="img/oxbanner.jpg" id="imageslider">
+                
+            </div>
+            <div class="carousel-item">
+                <img src="img/oxbanner.jpg" id="imageslider">
+                
+            </div>
+        </div>
+        <!-- Carousel controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+</header>
