c4cc6841b3913afc74e8a644d02cfe38d6fa866d
thantharhnin17 <thanthark88@gmail.com>
2020-07-03 16:31:25 -0800

    added footer file

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer id="footer" class="py-4 bg-dark text-white-50">
+    <div class="container-fluid text-center bg-primary">
+      <small>Copyright &copy; Your Website</small>
+    </div>
+  </footer>
